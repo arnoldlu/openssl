e619c641f114d6ebc8e67756132fbf1c199d4a52
Arnold Lu <arnoldlu@qq.com>
2017-04-07 14:20:04 +0800

    update run_openssl.sh
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

--- run_openssl.sh
+++ run_openssl.sh
@@ -4,7 +4,8 @@
 ./config -lcjson -lhttp_parser
 ./apps/openssl s_client -connect www.baidu.com:443
 #./apps/openssl s_k312 -connect 221.176.66.230:20001 -proxy proxynj.zte.com.cn:80
-#./apps/openssl s_client -connect 221.176.66.230:20001 -proxy proxynj.zte.com.cn:80
+#make && ./apps/openssl s_client -connect 221.176.66.230:20001 -proxy proxynj.zte.com.cn:80
+#make && ./apps/openssl s_client -connect b.fxltsbl.com:443 -proxy proxynj.zte.com.cn:80
 #sudo make install
 # make -j4 && sudo cp libssl.so.1.1 /usr/local/lib/ && sudo cp libcrypto.so.1.1 /usr/local/lib/
 
